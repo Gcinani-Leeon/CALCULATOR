4c7307209dff9d375e65654782b18c8c7df365fd
Gcinani.Leeon <gcinani.leeon011@gmail.com>
2025-06-29 02:19:50 +0200

    Initial commit

---
+++ js.js
@@ -0,0 +1,49 @@
+
+function allClear(x){
+    document.getElementById("screen").value = x;
+}
+
+// BUTTON PRESSED FUNCTION
+function button(btnPressed){
+    let currentScreenValue = document.getElementById("screen").value;
+
+    if (btnPressed == "0" && currentScreenValue == "0"){
+        document.getElementById("screen").value = "0";
+    }
+    else if (btnPressed !== "0" && currentScreenValue == "0"){
+        document.getElementById("screen").value = btnPressed;
+    }
+    else{
+        document.getElementById("screen").value = currentScreenValue + btnPressed;
+    }
+}
+
+let holdValue;
+let operaror;
+function Op(opPressed){
+    let currentScreenValue = document.getElementById("screen").value;
+
+    holdValue = parseInt(currentScreenValue);
+    operaror = opPressed;
+
+    document.getElementById("screen").value = 0;
+}
+
+function equalOp(equal){
+    let hold2 = parseInt(document.getElementById("screen").value);
+    let uns;
+    if(operaror == "*"){
+        uns = holdValue * hold2;
+    }
+    else if(operaror == "/"){
+        uns = holdValue / hold2;
+    }
+    else if(operaror == "-"){
+        uns = holdValue - hold2;
+    }
+    else if(operaror == "+"){
+        uns = holdValue + hold2;
+    }
+    document.getElementById("screen").value = uns;
+
+}
